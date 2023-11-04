82495cb3ac48eca7cc17bdb4ada7060756b3b238
Ameer khan B <106725517+itisameerkhan@users.noreply.github.com>
2023-11-04 14:27:24 +0530

    Update Demo.java

--- Bus Reservation/Demo.java
+++ Bus Reservation/Demo.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Demo {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.print("Demo File for pull request");
     System.out.println("Demo printing");
+    sc.close();
   }
 }
